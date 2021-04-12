05eee597fa0d8a059cf073b92f9aa04e69b54dc1
Neha Kennard <nnayak@cs.umass.edu>
2021-04-12 01:58:26 -0400

    more orda

---
+++ annotator/orda/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('assignments/<str:initials>/', views.assignments, name='assignments'),
+    path('rebuttal/<str:initials>/<str:rebuttal_id>/<int:index>/',
+         views.annotate_rebuttal,
+         name='ann_rebuttal'),
+    path('review/<str:initials>/<str:review_id>/',
+         views.annotate_review,
+         name='ann_review'),
+    path('agreement/', views.agreement, name='agreement'),
+    path('review_submitted/', views.review_submitted, name='review_submitted'),
+    path('rebuttal_submitted/', views.rebuttal_submitted, name='rebuttal_submitted'),
+]
