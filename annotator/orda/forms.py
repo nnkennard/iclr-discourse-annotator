0d4f6c9ba9e862a9e4a6126fa9eee8464ba6b08b
root <Neha Kennard>
2021-04-13 16:21:49 +0000

    longer forms allower

--- annotator/orda/forms.py
+++ annotator/orda/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class AnnotationForm(forms.Form):
-    annotation = forms.CharField(label='JSON annotation', max_length=5000)
+    annotation = forms.CharField(label='JSON annotation', max_length=20000)
 
 
 
